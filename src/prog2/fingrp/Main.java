d8665da96b021511691fba65f911238534527d32
Portia Caramat <146707717+PortiaCaramat@users.noreply.github.com>
2024-04-21 01:04:41 +0800

    first commit

--- src/prog2/fingrp/Main.java
+++ src/prog2/fingrp/Main.java
@@ -1,4 +1,10 @@
 package prog2.fingrp;
-
+import java.util.Scanner;
+import java.io.*;
 public class Main {
+    Scanner scan = new Scanner(System.in);
+    int choice;
+    // Load courses from file if exists
+
+
 }
