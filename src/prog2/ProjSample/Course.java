cc7ddde7c21a6b4c6b3683f1b082c1d54d6f1612
PokerFeces <barrozosean93@gmail.com>
2024-05-05 11:45:19 +0800

    first commit

---
+++ src/prog2/ProjSample/Course.java
@@ -0,0 +1,66 @@
+package prog2.ProjSample;
+
+import static java.lang.String.format;
+
+public class Course {
+
+	private int year;
+	private int term;
+	private String title;
+	private String code;
+	private int unit;
+
+	public Course() {
+		this.year= 1;
+		this.term= 1;
+		this.title = null;
+		this.code = null;
+		this.unit = 0;
+	}
+
+	public Course(int year, int term, String code, String title, int unit) {
+		this.year = year;
+		this.term = term;
+		this.title = title;
+		this.code = code;
+		this.unit = unit;
+	}
+
+
+	//setters
+
+	public void setYear(int year){this.year =year;}
+	public void setTerm(int term){this.term= term;}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public void setUnit(int unit) {
+		this.unit = unit;
+	}
+	//getters
+	public int getYear(){return year;}
+
+	public int getTerm(){return term;}
+	public String getTitle() {
+		return title;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public int getUnit() {
+		return unit;
+	}
+
+
+	@Override
+	public String toString() {
+		return format("%-20s%-100s%-22s%n", code,title,unit) ;
+	}
+}
