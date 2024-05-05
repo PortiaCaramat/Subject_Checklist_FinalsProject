6ba78044aea4fc756d8bf3ce5b296fe23b47d16e
PokerFeces <barrozosean93@gmail.com>
2024-05-05 11:45:01 +0800

    first commit

--- src/prog2/fingrp/Main.java
+++
@@ -1,9 +0,0 @@
-package prog2.fingrp;
-import java.util.Scanner;
-import java.io.*;
-public class Main {
-    static Course course = new Course("Discrete Structures", "9302", 3, 87.1, false);
-    public static void main(String[] args) {
-        System.out.println(course);
-    }
-}
