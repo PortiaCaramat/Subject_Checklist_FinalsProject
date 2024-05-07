d1ab66723278b357ed06716a200d99736be84fd9
Lyshael <148619987+Lyshael@users.noreply.github.com>
2024-05-08 00:36:24 +0800

    Add files via upload

--- src/prog2/fingrp/GUI.java
+++ src/prog2/fingrp/GUI.java
@@ -7,7 +7,8 @@ import java.lang.*;
 import java.io.*;
 import java.util.List;
 
-public class  GUI extends JFrame {
+public class
+GUI extends JFrame {
 
         public static String intToString (int term)  {
                 switch (term) {
@@ -58,16 +59,89 @@ public class  GUI extends JFrame {
             }
         }
 
+        public void displayAlphabetically(){
+            DatReadWrite<Course> courseRead = new DatReadWrite<>();
+            DatReadWrite<CourseGrade> persCourse = new DatReadWrite<>();
+
+            try {
+                List<Course> courses = courseRead.read("src\\prog2\\fingrp\\DATA\\Curriculum.dat");
+
+                // Sort the courses alphabetically by descriptive title
+                Collections.sort(courses, Comparator.comparing(Course::getTitle));
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Display courses in alphabetical order \n");
+                System.out.print("-------------------------------------------------------------------------------------------------------------------------------------------------\n");
+                System.out.println(courses.get(0));
+
+                for (int i = 1; i < courses.size(); i++) {
+                    if (courses.get(i).getTerm() != (courses.get(i - 1).getTerm())) {
+                    }
+                    System.out.println(courses.get(i));
+                }
+
+                scanner.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public void displayHighToLow(){
+
+        }
+
         public GUI() {
             //empty lmao
         }
 
         public static void main(String[] args) {
-            GUI program = new GUI();
+            try {
+                GUI program = new GUI();
 
-            program.showCurriculum();
+                Scanner kbd = new Scanner(System.in);
+                int choice = kbd.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        program.showCurriculum();
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+                        program.displayAlphabetically();
+                        break;
+                    case 8:
+
+                        break;
+                    case 9:
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Invalid choice");
+                        break;
+                }
+            } catch (InputMismatchException e){
+                System.out.println("Invalid input. Please enter  valid number");
+            } catch Exception e();
+            System.out.println("An error occurred" + e.getMessage());
+            e.printStackTrace();
         }
- }
+}
 
 
 
